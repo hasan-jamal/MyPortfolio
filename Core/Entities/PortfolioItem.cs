bf2f0ccbaa653c85c2156e300cc7307619817c0e
hasan-jamal <hasan0595488660@gmail.com>
2021-05-15 20:54:52 +0300

    Add project files.

---
+++ Core/Entities/PortfolioItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entities
+{
+    public class PortfolioItem
+    {
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+
+        public string ImageUrl  { get; set; }
+
+    }
+}
